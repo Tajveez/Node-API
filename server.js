54efc2beff0d57d442ddcfca20a1d84702283c2c
tajveez <tajvee786@gmail.com>
2020-10-21 11:16:05 -0700

    changes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => { })
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Hello</h1>')
+    res.end()
+    // res.send('hello')
+})
 const PORT = 5000
 
 server.listen(PORT,()=>console.log(`server running on PORT: ${PORT}`))
