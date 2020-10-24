cd40163320280ee20621d48d4dd746ce797f5321
tajveez <tajvee786@gmail.com>
2020-10-24 11:05:05 -0700

    MVC setup

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,35 @@
 const Product = require("../models/productModel");
 
+// @desc  Gets All Products
+// @route GET /api/product/
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(products));
-  } catch (error) {}
+  } catch (error) {
+    console.log("Controller Error: ", error);
+  }
 }
+
+// @desc  Gets All Product by Id
+// @route GET /api/product/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: `Product with Id ${id} not found.` }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log("Controller Error: ", error);
+  }
+}
+
+module.exports = {
+  getProducts,
+  getProduct,
+};
--- models/productModel.js
+++ models/productModel.js
@@ -2,10 +2,21 @@ const products = require("../data/products");
 
 function findAll() {
   return new Promise((resolve, reject) => {
-    reslove(products);
+    resolve(products);
+  });
+}
+
+function findById(id) {
+  return new Promise((resolve, reject) => {
+    resolve(
+      products.find((product) => {
+        return product.id === id;
+      })
+    );
   });
 }
 
 module.exports = {
   findAll,
+  findById,
 };
